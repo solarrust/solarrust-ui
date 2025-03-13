1c2ff986c2e18a6771450605b0387a0ef0d15082
Alena Batitskaia <batickaya.a@gmail.com>
2025-03-13 13:24:40 +0000

    Unnecessary files removed

--- packages/react-kit/dist/components/Button.d.ts
+++
@@ -1,5 +0,0 @@
-import React, { ButtonHTMLAttributes } from "react";
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-}
-export declare function Button({ children, ...props }: ButtonProps): React.JSX.Element;
-export {};
